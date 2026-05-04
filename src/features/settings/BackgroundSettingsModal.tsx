923bfa7772142907c58ee5f56da552f061f9f14c
Maksim <malutinm11@gmail.com>
2026-05-05 06:44:37 +0700

    fixed: excess import was deleted

--- src/features/settings/BackgroundSettingsModal.tsx
+++ src/features/settings/BackgroundSettingsModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Modal, message } from "antd";
 import { useAppDispatch } from "../../hooks";
 import { updateBackground } from "./settingsSlice";
